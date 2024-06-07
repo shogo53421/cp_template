b43f940b7365742366a1cf30e2e240db966c8293
shogo53421 <shogototo1004@docomo.ne.jp>
2024-06-07 13:55:31 +0900

    power

--- power.cpp
+++ power.cpp
@@ -3,4 +3,12 @@ using namespace std;
 
 long long power(long long a, long long b, long long m)
 {
+	long long r = 1;
+	a %= m;
+	for (int i = 0; i < b; i++)
+	{
+		r *= b;
+		r %= m;
+	}
+	return r;
 }
